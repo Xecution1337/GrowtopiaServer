2031e6df8544374c51e727faeb193c072c273629
Xecution1337 <76688105+Xecution1337@users.noreply.github.com>
2021-06-15 00:07:29 +0300

    Update enet server test.cpp

--- enet server test/enet server test.cpp
+++ enet server test/enet server test.cpp
@@ -321,6 +321,144 @@ vector<string> explode(const string &delimiter, const string &str)
 	return arr;
 }
 
+struct GamePacket
+{
+	BYTE* data;
+	int len;
+	int indexes;
+};
+
+
+GamePacket appendFloat(GamePacket p, float val)
+{
+	//p.data[56] += 1;
+	BYTE* n = new BYTE[p.len + 2 + 4];
+	memcpy(n, p.data, p.len);
+	delete p.data;
+	p.data = n;
+	n[p.len] = p.indexes;
+	n[p.len + 1] = 1;
+	memcpy(n + p.len + 2, &val, 4);
+	p.len = p.len + 2 + 4;
+	p.indexes++;
+	return p;
+}
+
+GamePacket appendFloat(GamePacket p, float val, float val2)
+{
+	//p.data[56] += 1;
+	BYTE* n = new BYTE[p.len + 2 + 8];
+	memcpy(n, p.data, p.len);
+	delete p.data;
+	p.data = n;
+	n[p.len] = p.indexes;
+	n[p.len + 1] = 3;
+	memcpy(n + p.len + 2, &val, 4);
+	memcpy(n + p.len + 6, &val2, 4);
+	p.len = p.len + 2 + 8;
+	p.indexes++;
+	return p;
+}
+
+GamePacket appendFloat(GamePacket p, float val, float val2, float val3)
+{
+	//p.data[56] += 1;
+	BYTE* n = new BYTE[p.len + 2 + 12];
+	memcpy(n, p.data, p.len);
+	delete p.data;
+	p.data = n;
+	n[p.len] = p.indexes;
+	n[p.len + 1] = 4;
+	memcpy(n + p.len + 2, &val, 4);
+	memcpy(n + p.len + 6, &val2, 4);
+	memcpy(n + p.len + 10, &val3, 4);
+	p.len = p.len + 2 + 12;
+	p.indexes++;
+	return p;
+}
+
+GamePacket appendInt(GamePacket p, int val)
+{
+	//p.data[56] += 1;
+	BYTE* n = new BYTE[p.len + 2 + 4];
+	memcpy(n, p.data, p.len);
+	delete p.data;
+	p.data = n;
+	n[p.len] = p.indexes;
+	n[p.len + 1] = 9;
+	memcpy(n + p.len + 2, &val, 4);
+	p.len = p.len + 2 + 4;
+	p.indexes++;
+	return p;
+}
+
+GamePacket appendIntx(GamePacket p, int val)
+{
+	//p.data[56] += 1;
+	BYTE* n = new BYTE[p.len + 2 + 4];
+	memcpy(n, p.data, p.len);
+	delete p.data;
+	p.data = n;
+	n[p.len] = p.indexes;
+	n[p.len + 1] = 5;
+	memcpy(n + p.len + 2, &val, 4);
+	p.len = p.len + 2 + 4;
+	p.indexes++;
+	return p;
+}
+
+GamePacket appendString(GamePacket p, string str)
+{
+	//p.data[56] += 1;
+	BYTE* n = new BYTE[p.len + 2 + str.length() + 4];
+	memcpy(n, p.data, p.len);
+	delete p.data;
+	p.data = n;
+	n[p.len] = p.indexes;
+	n[p.len + 1] = 2;
+	int sLen = str.length();
+	memcpy(n + p.len + 2, &sLen, 4);
+	memcpy(n + p.len + 6, str.c_str(), sLen);
+	p.len = p.len + 2 + str.length() + 4;
+	p.indexes++;
+	return p;
+}
+
+GamePacket createPacket()
+{
+	BYTE* data = new BYTE[61];
+	string asdf = "0400000001000000FFFFFFFF00000000080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+	for (int i = 0; i < asdf.length(); i += 2)
+	{
+		char x = ch2n(asdf[i]);
+		x = x << 4;
+		x += ch2n(asdf[i + 1]);
+		memcpy(data + (i / 2), &x, 1);
+		if (asdf.length() > 61 * 2) throw 0;
+	}
+	GamePacket packet;
+	packet.data = data;
+	packet.len = 61;
+	packet.indexes = 0;
+	return packet;
+}
+
+GamePacket packetEnd(GamePacket p)
+{
+	BYTE* n = new BYTE[p.len + 1];
+	memcpy(n, p.data, p.len);
+	delete p.data;
+	p.data = n;
+	char zero = 0;
+	memcpy(p.data + p.len, &zero, 1);
+	p.len += 1;
+	//*(int*)(p.data + 52) = p.len;
+	*(int*)(p.data + 56) = p.indexes;//p.len-60;//p.indexes;
+	*(BYTE*)(p.data + 60) = p.indexes;
+	//*(p.data + 57) = p.indexes;
+	return p;
+}
+
 struct gamepacket_t
 {
 private:
